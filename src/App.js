ee24c7283d45d7f26a7e63af2fcbecb917cf044b
mahesh0455 <mahesh.p.pawar045@gmail.com>
2021-07-29 11:43:07 +0530

    added new para

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div class="bgdark">
       <h1>Hello World</h1>
+      <p>Hello Universe</p>
       <input type="button" value="Click me" onClick="{myfunc}"></input>
     </div>
   );
