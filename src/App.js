c92561d69a5a5accb9c19af89a6f42c7bf281a05
Aditya <p.adi.salgude@gmail.com>
2021-07-29 11:25:58 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div class="bgdark">
+      <h1>Hello World</h1>
+      <input type="button" value="Click me" onClick="{myfunc}"></input>
+    </div>
+  );
+}
+
+export default App;
